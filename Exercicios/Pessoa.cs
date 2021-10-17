9a7b40abd036a51ea0ac982b04a3aee7f2b949cb
Alberto Araujo <albertoig8@hotmail.com>
2021-10-17 10:52:13 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicios/Pessoa.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    class Pessoa
+    {
+        public string nome;
+        public double imc;
+
+        public Pessoa(string nome)
+        {
+            this.setNome(nome);
+        }
+
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public double CalcularIMC(double peso, double altura)
+        {
+            double imc = peso / Math.Pow(altura, 2);
+
+            if(imc < 20)
+            {
+                Console.WriteLine("Seu IMC é {0}. Você esta abaixo do peso.", imc);
+            } else if(imc >= 20 && imc < 25)
+            {
+                Console.WriteLine("Seu IMC é {0}. Parabéns! Você esta no seu peso ideal!", imc);
+            } else
+            {
+                Console.WriteLine("Seu IMC é {0}. Você esta acima do peso.", imc);
+            }
+            return this.imc;
+        }
+    }
+}
---
+++ Exercicios/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao;
+            bool validacao = true;
+            double peso;
+            double altura;
+
+            Pessoa pessoa = new Pessoa("Alberto");
+
+
+            Console.WriteLine("Seja bem vindo ao Programa do Exercicio!");
+
+            do
+            {
+                Console.WriteLine("Escolha uma das opções:");
+                Console.WriteLine("(1) - Calcular IMC");
+
+                Console.WriteLine("(5) - Sair");
+
+                Console.WriteLine("Digite a sua opção: ");
+                opcao = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Digite o seu peso: ");
+                        peso = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Digite o seu Altura: ");
+                        altura = Convert.ToDouble(Console.ReadLine());
+
+                        pessoa.CalcularIMC(peso, altura);
+                        break;
+
+
+                    case 5:
+                        validacao = false;
+                        break;
+
+
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+                Console.WriteLine("Aperte Enter para continuar");
+                Console.ReadLine();
+                Console.Clear();
+            } while (validacao);
+        }
+    }
+}
